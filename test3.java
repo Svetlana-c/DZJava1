6e7eba7ff1fef11417caa62d44224266ce6d6df8
«Svetlana» <desko.nn@gmail.com>
2023-01-31 22:56:10 +0300

    Test3

--- test3.java
+++ test3.java
@@ -5,13 +5,15 @@ public class test3 {
         int x=new Random().nextInt(2000);
         int n = Integer.toBinaryString(x).length();
         int c = 0;
-        for (int i = Short.MIN_VALUE; i < x; i++){
+        for (int i = Short.MAX_VALUE; i < x; i++){
             if(i%n == 0) c++;
         }
         int [] m1 = new int[c];
         c = 0;
-        for(int i = Short.MIN_VALUE; i < x; i++){
+        for(int i = Short.MAX_VALUE; i < x; i++){
             if(i%n == 0) m1[c++] = i;
+        
         }
+    
     }
 }
