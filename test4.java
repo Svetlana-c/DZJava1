89cb65406606408c7fcc44c6eee533d823e79563
«Svetlana» <desko.nn@gmail.com>
2023-01-31 22:59:39 +0300

    Test4

---
+++ test4.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class test4 {
+    public static void main(String[] args){
+        int x=new Random().nextInt(2000);
+        int n = Integer.toBinaryString(x).length();
+        int c = 0;
+        for (int i = Short.MIN_VALUE; i < x; i++){
+            if(i%n != 0) c++;
+        }
+        int [] m1 = new int[c];
+        c = 0;
+        for(int i = Short.MIN_VALUE; i < x; i++){
+            if(i%n != 0) m1[c++] = i;
+        
+        }
+    
+    }
+    
+}
